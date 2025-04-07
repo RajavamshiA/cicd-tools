5138bd78660c0a49a643092261e8de2066bb6efc
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-04-07 14:49:57 +0530

    CICD pipeline

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ module "jenkins_agent" {
   name = "jenkins-agent"
 
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
+  vpc_security_group_ids = ["sg-090191332551d3d9b"]
   subnet_id = "subnet-0c9ef872d9e949944"
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins-agent.sh")
